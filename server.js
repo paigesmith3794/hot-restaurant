b614cefb39d0dfe49430a4e2eeb1fbb89fe753d6
paigesmith3794 <paigesmith3794@gmail.com>
2019-06-05 19:56:31 -0700

    added reservation data and routes, plus made a gitignore file

--- server.js
+++ server.js
@@ -12,10 +12,33 @@ var PORT = 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Reservation and Waitlist Data
+// =============================================================
+
+var reservations = [
+    {
+        name: "",
+        phoneNumber: 0,
+        email: "",
+        uniqueID: 0,
+    }
+];
+
+var waitlist = [
+    {
+        name: "",
+        phoneNumber: 0,
+        email: "",
+        uniqueID: 0,
+    }
+];
+
 
 // Routes
 // =============================================================
 
+// Route for reservation
+// =============================================================
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
@@ -25,42 +48,92 @@ app.get("/makereservation", function (req, res) {
     res.sendFile(path.join(__dirname, "make.html"));
 });
 
-// Displays all characters
-app.get("/api/characters", function (req, res) {
-    return res.json(characters);
+// Displays all reservations
+app.get("/api/reservations", function (req, res) {
+    return res.json(reservations);
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function (req, res) {
-    var chosen = req.params.character;
+// Displays a single reservations, or returns false
+app.get("/api/reservations/reservation", function (req, res) {
+    var chosen = req.params.reservations;
 
     console.log(chosen);
 
-    for (var i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-            return res.json(characters[i]);
+    for (var i = 0; i < reservations.length; i++) {
+        if (chosen === reservations[i].routeName) {
+            return res.json(reservations[i]);
         }
     }
 
     return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function (req, res) {
+// Create New Reservations - takes in JSON input
+app.post("/api/reservation", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newcharacter = req.body;
+    var newreservations = req.body;
 
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    newreservations.routeName = newreservations.name.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newcharacter);
+    console.log(newreservations);
 
-    characters.push(newcharacter);
+    reservations.push(newreservations);
 
-    res.json(newcharacter);
+    res.json(newreservations);
 });
+
+
+// Route for waitlist
+// =========================================================
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/makereservation", function (req, res) {
+    res.sendFile(path.join(__dirname, "make.html"));
+});
+
+// Displays all reservations
+app.get("/api/reservations", function (req, res) {
+    return res.json(reservations);
+});
+
+// Displays a single reservations, or returns false
+app.get("/api/reservations/reservation", function (req, res) {
+    var chosen = req.params.reservations;
+
+    console.log(chosen);
+
+    for (var i = 0; i < reservations.length; i++) {
+        if (chosen === reservations[i].routeName) {
+            return res.json(reservations[i]);
+        }
+    }
+
+    return res.json(false);
+});
+
+// Create New Reservations - takes in JSON input
+app.post("/api/reservation", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newreservations = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newreservations.routeName = newreservations.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newreservations);
+
+    reservations.push(newreservations);
+
+    res.json(newreservations);
+});
+
 
 // Starts the server to begin listening
 // =============================================================
